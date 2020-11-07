a5ae38a1f9aacf8fca217d4caf40b9fccd31aecd
Erik Lorenz <erikslorenz@gmail.com>
2020-11-07 01:24:51 -0800

    Upgraded to parcel v2. Next need to add the plugin styled components

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const LandingPage: React.FC = () => {
-  return <h1>This is the landing page.</h1>;
+  return <H1>This is the landing page.</H1>;
 };
 
 export default LandingPage;
+
+const H1 = styled.h1``;
